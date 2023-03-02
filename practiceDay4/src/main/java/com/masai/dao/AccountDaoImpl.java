c2602140aea641fb4790f8e95e77efbbba5cd3fb
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-02 15:21:56 +0530

    done

---
+++ practiceDay4/src/main/java/com/masai/dao/AccountDaoImpl.java
@@ -0,0 +1,98 @@
+package com.masai.dao;
+
+import javax.persistence.EntityManager;
+
+import com.masai.model.Account;
+import com.masai.utility.EMUtil;
+
+public class AccountDaoImpl implements AccountDao {
+
+	@Override
+	public boolean createAccount(Account account) {
+		boolean flag = false;
+		
+		EntityManager em = EMUtil.provideEntityManager();
+		
+		
+		em.getTransaction().begin();
+		em.persist(account);
+		em.getTransaction().commit();
+		flag=true;
+		em.close();
+		
+		return flag;
+	}
+
+	@Override
+	public boolean deleteAccount(int accno) {
+       boolean flag = false;
+       Account account = null;
+		
+		EntityManager em =   EMUtil.provideEntityManager();
+		
+		account =em.find(Account.class,accno);
+		
+		if(account != null) {
+			em.getTransaction().begin();
+			em.remove(account);
+			em.getTransaction().commit();
+			flag=true;
+		}
+		else {
+			System.out.println("account not found....");
+		}
+		em.close();
+		
+		
+		
+		
+		
+		return flag;
+	}
+
+	@Override
+	public boolean updateAccount(Account account) {
+		boolean flag = false;
+		
+		EntityManager em =   EMUtil.provideEntityManager();
+		
+		account =em.find(Account.class,account.getAccNo());
+		
+		if(account != null) {
+			em.getTransaction().begin();
+			em.merge(account);
+			em.getTransaction().commit();
+			flag=true;
+		}
+		else {
+			System.out.println("account not found....");
+		}
+		em.close();
+		
+		
+		
+		
+		
+		return flag;
+	}
+
+	@Override
+	public Account findAccount(int accno) {
+		Account acc = null;
+		EntityManager em = EMUtil.provideEntityManager();
+		acc=em.find(Account.class, accno);
+		
+		if(acc != null) {
+			return acc;
+		}else {
+			System.out.println("account not found with this accNo...");
+		}
+		
+		em.close();
+		
+		
+		
+		return acc;
+	}
+
+}
---
+++ practiceDay4/src/main/java/com/masai/usecase/CreateAccount.java
@@ -0,0 +1,34 @@
+package com.masai.usecase;
+
+import java.util.Scanner;
+
+import com.masai.dao.AccountDao;
+import com.masai.dao.AccountDaoImpl;
+import com.masai.model.Account;
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter account holder name...");
+		String name = sc.next();
+		
+		
+		System.out.println("enter account holder balance...");
+		int balance = sc.nextInt();
+		
+		Account acc = new Account();
+		acc.setName(name);
+		acc.setBalance(balance);
+		
+		AccountDao aDao = new AccountDaoImpl();
+		boolean b =aDao.createAccount(acc);
+		System.out.println(b);
+		
+		
+
+	}
+
+}
