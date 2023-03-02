a3fe29355524b0a95a3342de662033afcd5ef4a1
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-02 22:26:34 +0530

    done

---
+++ sb201Day5/src/main/java/com/masai/dao/EmployeeDaoImpl.java
@@ -0,0 +1,35 @@
+package com.masai.dao;
+
+import java.util.List;
+
+import com.masai.exception.EmployeeException;
+import com.masai.model.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	public List<Employee> getAllEmployees() throws EmployeeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Employee> getEmployeesByAddress(String address) throws EmployeeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Employee> getAllEmployeeWithRangeSalary(int startSalary, int endSalary) throws EmployeeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object[] getEmployeeNameAndSalary(int empId) throws EmployeeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getEmployeeSalaryById(int empId) throws EmployeeException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
