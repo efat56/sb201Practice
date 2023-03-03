12d43d89be937a31f2eb92664fd7b5cd3d49604a
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-03 16:08:32 +0530

    done

--- practiceDay4/src/main/java/com/masai/usecase/GetDetailsNonPk.java
+++ practiceDay4/src/main/java/com/masai/usecase/GetDetailsNonPk.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.masai.model.Account;
 import com.masai.utility.EMUtil;
@@ -22,12 +23,34 @@ public class GetDetailsNonPk {
 		List<Account> listAcc =  q.getResultList();
 		
 		
-//		for(Account a : listAcc) {
-//			System.out.println(a);
-//		}
+		for(Account a : listAcc) {
+			System.out.println(a);
+		}
 		
 		listAcc.forEach(a -> System.out.println(a));
-
+		
+		
+        // if we sure only one list will be come than we will use 
+		
+		
+		String jpql2 = "select a from Account a where a.name = 'name3' ";
+		
+		Query q2 =   em.createQuery(jpql2);
+		
+		Object obj =    q2.getSingleResult();
+		
+		Account acc = (Account)obj;
+		
+		Account acc2=(Account)q.getSingleResult();
+		
+		System.out.println(acc);
+		
+		
+		TypedQuery< Account> tq =     em.createQuery(jpql,Account.class);
+		
+		Account acc3 =   tq.getSingleResult();
+		
+		System.out.println(acc3);
 	}
 
 }
