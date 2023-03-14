cfe4898e687f67a58fcb1d34d147dea1a4418517
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-14 17:14:53 +0530

    create

---
+++ practiceDay5/src/main/java/com/Dao/AccountDaoImpl.java
@@ -0,0 +1,31 @@
+package com.Dao;
+
+import com.model.Account;
+
+public class AccountDaoImpl implements AccountDao {
+
+	@Override
+	public boolean createAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAccount(int accno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Account findAccount(int accno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
