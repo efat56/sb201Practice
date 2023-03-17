520bf2be085c2634818c93d286000efecc3769b1
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-17 19:20:44 +0530

    done

---
+++ practiceDay5/src/main/java/com/usecase/DeleteAccount.java
@@ -0,0 +1,27 @@
+package com.usecase;
+
+import java.util.Scanner;
+
+import com.Dao.AccountDao;
+import com.Dao.AccountDaoImpl;
+import com.model.Account;
+
+public class DeleteAccount {
+
+	public static void main(String[] args) {
+
+	      Scanner sc = new Scanner(System.in);
+	         
+	         System.out.println("enter account number...");
+	 		int accNo= sc.nextInt();
+	 		
+	 		
+	 		
+	 		AccountDao aDao = new AccountDaoImpl();
+	 		boolean b = aDao.deleteAccount(accNo);
+	 		System.out.println(b);
+
+
+	}
+
+}
