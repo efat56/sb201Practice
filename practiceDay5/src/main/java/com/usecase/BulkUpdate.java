1c86f16cf0d2155036c9a93188491c52c355a67b
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-17 22:58:47 +0530

    done

--- practiceDay5/src/main/java/com/usecase/BulkUpdate.java
+++ practiceDay5/src/main/java/com/usecase/BulkUpdate.java
@@ -1,17 +1,13 @@
 package com.usecase;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import com.EMUtility.EMUtil;
-import com.model.Account;
 
 public class BulkUpdate {
 
 	public static void main(String[] args) {
-		
 		EntityManager em = EMUtil.ProvideEntityManger();
 		
 		String jpql = "update Account set balance=balance+500";
@@ -23,13 +19,7 @@ public class BulkUpdate {
 		em.getTransaction().commit();
 		
 		System.out.println("row updated " +x);
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
