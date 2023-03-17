3abb1d4ddeae8facad1ed847a67ca160a0b5c9ac
efat56 <104213039+efat56@users.noreply.github.com>
2023-03-17 22:31:25 +0530

    done

--- practiceDay5/src/main/java/com/usecase/SearchOnNonPk.java
+++ practiceDay5/src/main/java/com/usecase/SearchOnNonPk.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.EMUtility.EMUtil;
 import com.model.Account;
@@ -14,16 +15,30 @@ public class SearchOnNonPk {
 		
 		EntityManager em =   EMUtil.ProvideEntityManger();
 		
-		String jpql = "select a from Account a where a.name = 'name1' ";
+		String jpql = "select a from Account a where a.name = 'name2' ";
 		
 		 Query q = em.createQuery(jpql);
+		 TypedQuery<Account> q2 = em.createQuery(jpql,Account.class);
 		 
 		 List<Account> list = q.getResultList();
 		 
 		 for(Account acc : list) {
 			 System.out.println(acc);
 		 }
+		 
+		 
+		 // if we confirm only one result will come....
+		 
+		 Object obj = q.getSingleResult();
+		 Account acc =(Account)obj;
+		 
+		Account a=(Account) q.getSingleResult();
+		System.out.println(a);
+		 
+		Account acc =  q2.getSingleResult();
+		System.out.println(acc);
 
+		 
 
 
 	}
